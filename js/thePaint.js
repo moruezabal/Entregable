407c315d8e1937a13418a4891f7e95d45ab5ab70
Dolores Pena <dolorespenadecristofaro@gmail.com>
2021-09-12 23:50:02 -0300

    cambios en js sobre el lapiz

--- js/thePaint.js
+++ js/thePaint.js
@@ -3,14 +3,15 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
+
 context.lineWidth = 1;//ancho de linea
 
 let ruta = false; //si se movio el mouse
 
 function draw(event){// funcion dibujar
 
-    let x = event.clientX; // posicion x del mouse
-    let y = event.clientY; // posicion y del mouse
+    let x = event.clientX - canvas.offsetLeft; // posicion x del mouse
+    let y = event.clientY - canvas.offsetTop; // posicion y del mouse
 
     if(ruta == true){
         context.lineTo(x,y); // hacer linea al x,y
@@ -40,6 +41,9 @@ function lineWidth(ancho){ //funcion para el ancho de la linea
     document.getElementById("value").innerHTML = ancho.value;
 }
 
-function cleanUp(){ // funcion borrar/limpiar
-    context.cleanReact(0,0,canvas.width, canvas.height); // se le pasa las coordenadas iniciales (0 en x y 0 en y) y el ancho y alto final del canvas
+function cleanUpAll(){ // funcion borrar/limpiar todo
+    context.clearRect(0,0,canvas.width, canvas.height); // se le pasa las coordenadas iniciales (0 en x y 0 en y) y el ancho y alto final del canvas
+}
+function cleanUp(){ // esta funcion se usara como goma.. pinta con blanco
+
 }
