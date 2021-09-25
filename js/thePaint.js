8cd45fd6becc82d604002fc696e02240aa2246fb
Mauricio Oruezabal <moruezabal@gmail.com>
2021-09-24 22:08:28 -0300

    Detección de bordes

--- js/thePaint.js
+++ js/thePaint.js
@@ -11,6 +11,7 @@ let btnSepia = document.getElementById("sepia");
 let btnContrast = document.getElementById("contrast");
 let btnBlur = document.getElementById("blur");
 let btnSave = document.getElementById("save");
+let btnSaturation = document.getElementById("saturation");
 
 let loadButton = document.getElementById('loadButton');
 
@@ -204,6 +205,53 @@ loadButton.addEventListener('change', function(ev) {
     }
 });
 
+/**
+* @param data - input pixels data
+* @param idx - the index of the central pixel
+* @param w - image width (width*4 in case of RGBA)
+* @param m - the gradient mask (for Sobel=[1, 2, 1])
+*/
+function conv3x(data, idx, w, m){
+    return (m[0]*data[idx - w - 4] + m[1]*data[idx - 4] + m[2]*data[idx + w - 4]
+        -m[0]*data[idx - w + 4] - m[1]*data[idx + 4] - m[2]*data[idx + 4 + 4]);
+  }
+  
+  function conv3y(data, idx, w, m){
+    return (m[0]*data[idx - w - 4] + m[1]*data[idx - w] + m[2]*data[idx - w + 4]
+        -(m[0]*data[idx + w - 4] + m[1]*data[idx + w] + m[2]*data[idx + w + 4]));
+  }
+  
+  
+  /**
+  * @param pixels - Object of image parameters
+  * @param mask - gradient operator e.g. Prewitt, Sobel, Scharr, etc. 
+  */
+  function gradient_internal(pixels, mask)
+  {
+    var data = pixels.data;
+    var w = pixels.width*4;
+    var l = data.length - w - 4;
+    var buff = new data.constructor(new ArrayBuffer(data.length));
+    
+    for (var i = w + 4; i < l; i+=4){
+      var dx = conv3x(data, i, w, mask);
+      var dy = conv3y(data, i, w, mask);
+      buff[i] = buff[i + 1] = buff[i + 2] = Math.sqrt(dx*dx + dy*dy);
+      buff[i + 3] = 255;
+    }
+    pixels.data.set(buff);
+  }
+  
+  /**
+  * @param canvas - HTML5 Canvas elementFromPoint
+  */
+  function gradient(){
+    var context = canvas.getContext('2d');
+    var pixels = context.getImageData(0, 0, canvas.width,canvas.height);
+    gradient_internal(pixels, [1, 2, 1]); // Apply Sobel operator
+    context.putImageData(pixels, 0, 0);
+  }
+
 btnBorrarTodo.addEventListener("click", cleanUpAll);
 btnBorrar.addEventListener("click", cleanUp);
 
@@ -213,3 +261,6 @@ btnSepia.addEventListener("click", sepiaImage);
 btnContrast.addEventListener("click", contrastImage);
 btnBlur.addEventListener("click", blurImage);
 btnSave.addEventListener("click", saveImage);
+btnSaturation.addEventListener("click", gradient);
+
+
